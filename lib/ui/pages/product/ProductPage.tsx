2a56fc7669b230e0d15c165bdee764ad2ddeaa59
Ayomide Oguntokun <ay884120@dal.ca>
2025-06-26 12:45:45 -0300

    product Description

--- lib/ui/pages/product/ProductPage.tsx
+++ lib/ui/pages/product/ProductPage.tsx
@@ -5,6 +5,15 @@ const productName = "Beach Umbrella - Sunshine Yellow"
 
 export default function ProductPage(){
     return (
-        <h2></h2>
+        <div>
+            <p>100% UV Protection & Waterproof Material: Shopizon beach umbrella is made of polyester providing you to UPF 50+ protection so as to block harmful radiation in</p>
+            <p> summer. UV-Resistant</p>
+            <p>Long lasting, Fade-Resistant and Easy to clean</p>
+            <p>Sturdy & Stable Frame: 8 sturdy fibreglass ribs providing strong support against wind</p>
+            <p>Solid sand anchor alloow this large umbrella more sturdy and stable</p>
+            <p>Unique Air-vented canopy: canopy designed to facilitate air flow for more stability. Vents on the top of beach umbrella is to promote air flow, so that umbrellas for beach to</p>
+            <p> combat high winds</p>
+
+        </div>
     );
 }
